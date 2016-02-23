5c504646cd4ad5f9d380f731965d3cf16284a487
Ben Moshe Sapo <bensapo@gmail.com>
2016-02-23 14:36:27 +0200

    Created class and added contructor

---
+++ Fractions/Tut2-Fractions.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+class Fraction{
+private:
+	int denom;
+	int num;
+public:
+	Fraction(int denominator = 1, int numerator = 1){
+		denom = denominator;
+		num = numerator;
+	}
+
+};
