c03f87240cad77121b64779da484ef386801ceef
Ben Moshe Sapo <bensapo@gmail.com>
2016-02-23 15:14:45 +0200

    Add, LCM , get and set functions

--- Fractions/Tut2-Fractions.cpp
+++ Fractions/Tut2-Fractions.cpp
@@ -6,10 +6,57 @@ class Fraction{
 private:
 	int denom;
 	int num;
+
+	int LCM(int num1, int num2, int count=1){
+		if (count % num1 == 0 && count % num2 == 0){
+			return count;
+		}
+		else{
+			LCM(num1, num2, count+1);
+		}
+	}
 public:
-	Fraction(int denominator = 1, int numerator = 1){
+	Fraction(int numerator = 1, int denominator = 1){
 		denom = denominator;
 		num = numerator;
 	}
 
+	void SetNum(int numerator){
+		num = numerator;
+	}
+
+	void SetDenom(int denominator){
+		denom = denominator;
+	}
+
+	int GetNum(){
+		return num;
+	}
+
+	int GetDenom(){
+		return denom;
+	}
+	Fraction add(int Num2, int Denom2){
+		int lcm = LCM(denom, Denom2);
+		Num2 *= lcm/Denom2;
+		num *= lcm/denom;
+		Fraction Result(num + Num2 , lcm);
+		return Result;
+	}
+
+	void Print(){
+		cout << num << "/" << denom << endl;
+	}
+
+	~Fraction(){
+
+	}
 };
+
+int main(){
+	Fraction First(1, 2);
+	First.Print();
+	Fraction Result;
+	Result = First.add(3, 4);
+	Result.Print();
+}
